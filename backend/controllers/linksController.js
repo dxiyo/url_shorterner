b966297f3021e0ee11c8a61d3a003efbfe0af48b
Abdullah Al-Qadmi <qadm4u@gmail.com>
2022-02-12 17:22:50 +0300

    renamed some functions and variables for clarity and now the visits of each short link is detected

--- backend/controllers/linksController.js
+++ backend/controllers/linksController.js
@@ -6,19 +6,20 @@ export const addLink = async (req, res) => {
     const link = req.body.url
 
     const subDomain = Links.hasSubDomain(link) ? Links.getSubDomainInitial(link) : ''
+    const domain = Links.getDomain(link)
     const subFolders = Links.hasSubFolders(link) ? Links.getSubFolderInitials(link) : ''
     // if there's no subdomain or subfolder, takes only the domain name from the link. if there is either a subdomain or a subfolder, only use the initial of the domain.
-    const domain = subDomain + Links.getDomain(link) + subFolders
+    const shortLink = subDomain + domain + subFolders
 
-    if( await DB.isMemberOfDomainSet(domain) === 1) {
+    if( await DB.isMemberOfShortLinkSet(shortLink) === 1) {
         res.json({
             message: "this link is already inserted."
         })
         return
     }
-    DB.addDomainToSet(domain)
+    DB.addShortLinkToSet(shortLink)
     
-    const reply = await DB.setLinkHash(domain, link)
+    const reply = await DB.setLinkHash(shortLink, link)
     
     res.json({
         message: "link added successfuly!",
@@ -28,8 +29,10 @@ export const addLink = async (req, res) => {
 
 // redirects the short link to the real link
 export const getLink = async (req, res) => {
-    const domain = req.params.url
-    const link = await DB.getLinkHash(domain)
+    const shortLink = req.params.url
+    // increment the number of visits
+    await DB.incrementLinkVisits(shortLink)
+    const link = await DB.getLinkHash(shortLink)
     res.redirect(link)
 }
 
--- backend/models/Database.js
+++ backend/models/Database.js
@@ -7,29 +7,32 @@ const redis = new Redis({
     port: process.env.DB_PORT,
     password: process.env.PASSWORD
 });
-
-// add an item to the domains set (its like an array in redis)
-export const addDomainToSet = async domain => await redis.sadd(`domains`, domain)
+// addDomainToSet => addShortLinkToSet
+// add an item to the short links set (its like an array in redis)
+export const addShortLinkToSet = async shortLink => await redis.sadd(`shortLinks`, shortLink)
 // get all the items
-export const getAllDomains = async () => await redis.smembers('domains')
+export const getAllShortLinks = async () => await redis.smembers('shortLinks')
 
 // return 0 or 1
-export const isMemberOfDomainSet = async domain => await redis.sismember('domains', domain)
+export const isMemberOfShortLinkSet = async shortLink => await redis.sismember('shortLinks', shortLink)
 
-// sets a hash (like an object in redis) where the identifier is the domain name, "longLink" is the key, and the value is the full link
-export const setLinkHash = async ( domain, link ) => await redis.hset(`links:${domain}`, 'longLink', link)
+// sets a hash (like an object in redis) where the identifier is the shortLink name, "longLink" is the key, and the value is the full link. "visits" is another key with a default 0 value.
+export const setLinkHash = async ( shortLink, link ) => await redis.hmset(shortLink, 'longLink', link, 'visits', 0)
 
 // gets a specific hash (again like an object) by the id and asks for the key you want (there's only longLink in these objects anyway)
-export const getLinkHash = async domain => await redis.hget(`links:${domain}`, 'longLink')
+export const getLinkHash = async shortLink => await redis.hget(shortLink, 'longLink')
 
-// as far as i can tell, redis doesn't have the ability to store an array of objects, or a set of hashes more accurately. this function creates that. it loops over the domain set and uses the name of each item to get the hash with the same id name. it pushes the hash and the domain name as an object into an array and returns a full array of objects.
+// increment the visits
+export const incrementLinkVisits = async shortLink => await redis.hincrby(shortLink, visits, 1)
+
+// as far as i can tell, redis doesn't have the ability to store an array of objects, or a set of hashes more accurately. this function creates that. it loops over the shortLink set and uses the name of each item to get the hash with the same id name. it pushes the hash and the shortLink name as an object into an array and returns a full array of objects.
 export const storeAllLinksInArrayOfObjects = async () => {
-    const domains = await getAllDomains()
+    const shortLinks = await getAllShortLinks()
     let links = []
-    for ( const domain of domains ) {
-        const link = await getLinkHash(domain)
+    for ( const shortLink of shortLinks ) {
+        const link = await getLinkHash(shortLink)
         links.push({
-            shortLink: `http://localhost:5000/${domain}`,
+            shortLink: `http://localhost:5000/${shortLink}`,
             longLink: link
         })
     }
